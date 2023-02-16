5ca8405bb068414e963ac8e76ac2db5f7b17c34f
geektcp <geektcp@163.com>
2023-02-17 01:32:50 +0800

    code review: ThyEncrypt

--- src/main/java/com/geektcp/common/core/system/ThyEncrypt.java
+++ src/main/java/com/geektcp/common/core/system/ThyEncrypt.java
@@ -29,42 +29,56 @@ class ThyEncrypt {
     private ThyEncrypt() {
     }
 
+    /**
+     * PrivateKey
+     * openssl genrsa -out rsa_private_key.pem 1024
+     * openssl pkcs8 -topk8 -inform PEM -in rsa_private_key.pem -outform PEM -out rsa_private_key_01.pem -nocrypt
+     * cat rsa_private_key_01.pem
+     */
+    private static final String PRIVATE_KEY =
+            "MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAMFvz2d3fWA0keI5" +
+            "l2EC5GlfZ6pTSoQRIT2dlv9RchI7wwxCKcAShH6LzByLra+hJGIRlL5700gxgOju" +
+            "hDpLyuIUleHv1uMQ5Qc5HnauNrDOzFtbel9v7F/Gy6qcejCJfAQYdnpjzWwkRKeL" +
+            "TcKDN3R2Hhm3HSMBFvtuuamBlYDDAgMBAAECgYEAlsrjwwO6IBI7G0DMClsn1iEC" +
+            "fhu5/iQgZpkACf7izuX5kgfN5iZJVSEDt8kHV99lrnGII8oBlcjJdkvhllRZTfEV" +
+            "JGLJW/gnGl0fH812K+MdSPHsFh2z9h51go/WOeDeN5zC+jheYvAovh3v5C6uWmEb" +
+            "S6n6mBkbZ2mt9418d3ECQQD8Ly0A8ul/EQR9fr8bUnMyn47vnZTiUPL8vKLh0XlM" +
+            "+YLKp0UvG1NT8gTAArycWpKhy9R9YDAt6li8C8J0NJ67AkEAxF0UHB/3VlV1b3nz" +
+            "Wbe+ES7MLL2O7hDcRh1Q6TsnmDAk5hJRg/bqIoSArT91FwoijCGpQtOVDE+LKqHp" +
+            "DzE5mQJBAJHuKri12HBzRInqYmRJHehZdLksEw+zkCi4b/kE4pCsggcLLHnJ8jpN" +
+            "fnouGz7PGrMN5HR4yOiJB7gphekA2ikCQDhl32vAhqKGQwd0iD9hdbarsTq8avvX" +
+            "XdzJeL60HcgpM7/czQrQ9Sha1DZuPmwSnh+PX3TxHiL+CaOCA1U4tykCQQDnwAjl" +
+            "Ih+D++1ZFP7eNkdsvxZEzUk89IiLz2UXzt1IusYT+gLiC2+H89xKKGdcw1bZQ+op" +
+            "3DgQhwv64DElRA2N";
+
+    /**
+     * PublicKey
+     * openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
+     * cat rsa_public_key.pem
+     */
+    private static final String PUBLIC_KEY =
+            "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBb89nd31gNJHiOZdhAuRpX2eq" +
+            "U0qEESE9nZb/UXISO8MMQinAEoR+i8wci62voSRiEZS+e9NIMYDo7oQ6S8riFJXh" +
+            "79bjEOUHOR52rjawzsxbW3pfb+xfxsuqnHowiXwEGHZ6Y81sJESni03Cgzd0dh4Z" +
+            "tx0jARb7brmpgZWAwwIDAQAB";
+
     private static final String PARAM = "thy$p*r^#a>s!m#.x@";
     private static final String DES_KEY = "thy$k*e^#>y!#.x@";
     private static final String ALGORITHM = "AES/GCM/NoPadding";
 
     private static Cipher cipher;
     private static RSA rsa;
-
+    private static ThyEncrypt instance;
     private static IvParameterSpec iv = new IvParameterSpec(PARAM.getBytes(StandardCharsets.UTF_8));
 
-    /**
-     * PrivateKey
-     * openssl genrsa -out rsa_private_key.pem 1024
-     * openssl pkcs8 -topk8 -inform PEM -in rsa_private_key.pem -outform PEM -out rsa_private_key_01.pem -nocrypt
-     * cat rsa_private_key_01.pem
-     * <p>
-     * PublicKey
-     * openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
-     * cat rsa_public_key.pem
-     */
-    private static RSA init() {
-        String keysDir = Sys.getResourceRootPath() + "keys";
-        String privateKeyPath = keysDir + "/PrivateKey";
-        String publicKeyPath = keysDir + "/PublicKey";
-        String privateKey = Sys.readPrivateKeyFile(privateKeyPath);
-        String publicKey = Sys.readPublicKeyFile(publicKeyPath);
-        rsa = new RSA(privateKey, publicKey);
-        return rsa;
-    }
 
-    public static RSA getInstance() {
-        if (Objects.isNull(rsa)) {
-            return init();
+
+    public static ThyEncrypt getInstance() {
+        if (Objects.isNull(instance)) {
+            return new ThyEncrypt();
         }
-        return rsa;
+        return instance;
     }
-
 
     public static String md5(String source) {
         String result = "";
@@ -77,9 +91,6 @@ class ThyEncrypt {
             StringBuilder sign = new StringBuilder();
             for (byte b : bytes) {
                 int bt = b & 0xff;
-                if (bt < 0) {
-                    bt += 256;
-                }
                 if (bt < 16) {
                     sign.append(0);
                 }
@@ -93,7 +104,7 @@ class ThyEncrypt {
     }
 
     public static String encrypt(String str) {
-        RSA rsa = ThyEncrypt.getInstance();
+        RSA rsa = ThyEncrypt.getRSA();
         byte[] encrypt1 = rsa.encrypt(StrUtil.bytes(str, CharsetUtil.CHARSET_UTF_8), KeyType.PublicKey);
         return HexUtil.encodeHexStr(encrypt1);
     }
@@ -103,7 +114,7 @@ class ThyEncrypt {
             return null;
         }
         try {
-            RSA rsa = ThyEncrypt.getInstance();
+            RSA rsa = ThyEncrypt.getRSA();
             byte[] bytesPassword = rsa.decrypt(str, KeyType.PrivateKey);
             return StringUtils.toEncodedString(bytesPassword, CharsetUtil.CHARSET_UTF_8);
         } catch (Exception e) {
@@ -140,13 +151,24 @@ class ThyEncrypt {
         }
     }
 
-
     public static Key buildKey(String secret) {
         byte[] keyBytes = Decoders.BASE64.decode(secret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    ///////////////////////////////////////////////
+    //////////////////// private function ///////////////////////////
+    private static RSA init() {
+        rsa = new RSA(PRIVATE_KEY, PUBLIC_KEY);
+        return rsa;
+    }
+
+    public static RSA getRSA() {
+        if (Objects.isNull(rsa)) {
+            return init();
+        }
+        return rsa;
+    }
+
     private static DESKeySpec getDesKeySpec(String source) throws GeneralSecurityException {
         if (source == null || source.length() == 0) {
             return null;
---
+++ src/test/java/com/geektcp/common/core/system/ThyEncryptTest.java
@@ -0,0 +1,24 @@
+package com.geektcp.common.core.system;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author geektcp on 2023/2/15 0:27.
+ */
+public class ThyEncryptTest {
+
+
+    @Test
+    public void test() {
+        Sys.p(Sys.encrypt("1111"));
+        Sys.p(Sys.decrypt(
+                "0ff2aaffd3ecf1b3b24dd04b6046424351b6038c8136286a2738c2ec7d0fc5988b1f6a23da0cd" +
+                        "5a44fb2e74b2bf63dd5c40adf4965ca1df0bd38838b0718b3f287aed7cd945235dc1cc7c8bb" +
+                        "bb3775aee2a9157a5299a2151e4c691fd7f4d1b3e3dad04aa6429d4db67092711158aac1eb6" +
+                        "ec06f2ee0133cd05f7a4b75487939"));
+        Assert.assertTrue(true);
+    }
+
+}
