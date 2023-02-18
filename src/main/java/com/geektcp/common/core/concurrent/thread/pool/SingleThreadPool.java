8fa1894458d73295527c3add9e4c6ac9c1d9a9c6
geektcp <geektcp@163.com>
2023-02-18 23:46:42 +0800

    review

--- src/main/java/com/geektcp/common/core/concurrent/thread/pool/SingleThreadPool.java
+++
@@ -1,8 +0,0 @@
-package com.geektcp.common.core.concurrent.thread.pool;
-
-/**
- * @author geektcp on 2023/2/2 0:00.
- */
-public class SingleThreadPool {
-
-}
--- src/test/java/com/geektcp/common/core/concurrent/thread/executor/TinyExecutorBuilderTest.java
+++ src/test/java/com/geektcp/common/core/concurrent/thread/executor/TinyExecutorBuilderTest.java
@@ -1,10 +1,7 @@
 package com.geektcp.common.core.concurrent.thread.executor;
 
-import com.geektcp.common.core.system.Sys;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.concurrent.ExecutorService;
 
 /**
  * @author geektcp on 2023/2/18 23:22.
