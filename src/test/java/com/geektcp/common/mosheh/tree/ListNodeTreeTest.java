34c9950584e454e79f4ff7d46f978edf8db4d668
geektcp <geektcp@gmail.com>
2023-08-24 19:34:36 +0800

    review

--- src/test/java/com/geektcp/common/mosheh/tree/ListNodeTreeTest.java
+++ src/test/java/com/geektcp/common/mosheh/tree/ListNodeTreeTest.java
@@ -16,44 +16,44 @@ public class ListNodeTreeTest {
 
         UserDirVo<String> vo1 = new UserDirVo<>();
         vo1.setTitle("xxx");
-        vo1.setId("111");
-        vo1.setParentId(null);
+        vo1.setNodeId("111");
+        vo1.setParentNodeId(null);
         voList.add(vo1);
 
         UserDirVo<String> vo2 = new UserDirVo<>();
         vo2.setTitle("xxx");
-        vo2.setId("222");
-        vo2.setParentId(null);
+        vo2.setNodeId("222");
+        vo2.setParentNodeId(null);
         voList.add(vo2);
 
         UserDirVo<String> vo3 = new UserDirVo<>();
         vo3.setTitle("xxx");
-        vo3.setId("333");
-        vo3.setParentId("111");
+        vo3.setNodeId("333");
+        vo3.setParentNodeId("111");
         voList.add(vo3);
 
         UserDirVo<String> vo4 = new UserDirVo<>();
 //        vo4.setTitle("xxx");
-        vo4.setId("444");
-        vo4.setParentId("222");
+        vo4.setNodeId("444");
+        vo4.setParentNodeId("222");
         voList.add(vo4);
 
         UserDirVo<String> vo5 = new UserDirVo<>();
         vo5.setTitle("xxx");
-        vo5.setId("555");
-        vo5.setParentId("333");
+        vo5.setNodeId("555");
+        vo5.setParentNodeId("333");
         voList.add(vo5);
 
         UserDirVo<String> vo6 = new UserDirVo<>();
         vo6.setTitle("xxx");
-        vo6.setId("666");
-        vo6.setParentId("444");
+        vo6.setNodeId("666");
+        vo6.setParentNodeId("444");
         voList.add(vo6);
 
         UserDirVo<String> vo7 = new UserDirVo<>();
         vo7.setTitle("xxx");
-        vo7.setId("777");
-        vo7.setParentId("555");
+        vo7.setNodeId("777");
+        vo7.setParentNodeId("555");
         voList.add(vo7);
 
 
