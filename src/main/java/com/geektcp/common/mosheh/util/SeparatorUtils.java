44f64ca04b07e4f0dd26d4be8eb3b51c43dd78de
geektcp <geektcp@gmail.com>
2023-09-14 00:33:17 +0800

    ADD SeparatorUtils

---
+++ src/main/java/com/geektcp/common/mosheh/util/SeparatorUtils.java
@@ -0,0 +1,13 @@
+package com.geektcp.common.mosheh.util;
+
+/**
+ * @author geektcp on 2023/9/14 0:29.
+ */
+public class SeparatorUtils {
+
+    public static final String SEPARATOR_LOG = "|";
+    public static final String SEPARATOR_DIR = "/";
+    public static final String SEPARATOR_WORD = ",";
+    public static final String SEPARATOR_STRING = "_";
+
+}
